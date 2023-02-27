1ace90c4675dd4542a84387e8b77b4cefab6a5e2
greyforrest <melanie.voellmin@gmail.com>
2023-02-27 11:12:01 +0100

    Firebase Connection, Events

---
+++ KalterKrieg/Data/Ereignis.cs
@@ -0,0 +1,11 @@
+﻿namespace KalterKrieg.Data
+{
+    public class Ereignis
+    {
+        public string name { get; set; }
+        public string dateStart { get; set; }
+        public string dateEnd { get; set; }
+        public string shortSummary { get; set; }
+
+    }
+}
---
+++ KalterKrieg/Data/Firebase.cs
@@ -0,0 +1,22 @@
+﻿using Firebase.Database
+
+namespace KalterKrieg.Data
+{
+    public class Firebase
+    {
+        FirebaseClient client = new FirebaseClient("https://projekt-kalter-krieg-default-rtdb.europe-west1.firebasedatabase.app/");
+
+        public async Task<List<Ereignis>> getAllEreignisseSorted()
+        {
+            var events = await client.Child("events").OnceAsync<Ereignis>();
+
+            return events?.Select(e => new Ereignis()
+            {
+                name = e.Key,
+                dateStart = e.Object.dateStart,
+                dateEnd = e.Object.dateEnd,
+                shortSummary = e.Object.shortSummary
+            }).ToList();
+        }
+    }
+}
